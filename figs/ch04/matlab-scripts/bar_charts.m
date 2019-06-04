e033e980d8e46ebfd291052401e62e6b197ac2e0
Laura Maguire <laura.maguire@colorado.edu>
2019-06-04 12:32:11 -0600

    Proofread and tidied everything except abstract and chapter 4. Optimized large .svg files and organized figure source files.

--- figs/ch04/matlab-scripts/bar_charts.m
+++ figs/ch04/matlab-scripts/bar_charts.m
@@ -78,3 +78,108 @@ hold off
 xticklabels({'','No FSFG ','FSFG concat-1','FSFG concat-2',''})
 title('Partition coefficient');
 legend({'Transport factor','Inert protein'});
+
+%% Bound diffusion with predictions, upper and lower bounds
+figure(1)
+hold on
+
+hb = bar(vertcat([0.40 4.25 5.62],[0.59 5.18 5.22]));
+errorbar(0.5*(1:6),[0.40 4.25 5.62 0.59 5.18 5.22],[0.1 1 2.27 0.06 0.5 3.20],'k.')
+axis([0 3 0 10])
+
+hold off
+ set(hb(1), 'FaceColor','r')
+%  set(hb(2), 'FaceColor','b')
+% set(hb(3), 'FaceColor','g')
+%axis([0 3 0 10])
+xticklabels({'','','FSFG concat-1','','FSFG concat-2',''})
+title('Bound diffusion constant ($\mu$m/s$^2$)');
+legend({'Model prediction','Experiment'});
+%% Plot of tethered diffusion prediction DB vs KD
+kd = logspace(1, 3); % in uM
+kon = 1e9*1e-6; %in uM^-1 s^-1;
+lp = 1e-3; % in um
+
+% plot cct1 values
+DF = 35; % in um^2/s
+Lc = 50e-3; % in um
+DB = DF./(1+3.*DF./(Lc.*lp.*kon.*kd));
+semilogx(kd, DB)
+
+% plot cct2 values
+DF = 41; % in um^2/s
+Lc = 100e-3; % in um
+DB = DF./(1+3.*DF./(Lc.*lp.*kon.*kd));
+hold on
+semilogx(kd, DB)
+hold off
+
+% cct1 KD lower bound
+line([24 24], [0 20]);
+% cct1 KD upper bound
+line([300 300], [0 20]);
+% cct2 KD lower bound
+line([18 18], [0 20]);
+% cct2 KD upper bound
+line([180 180], [0 20]);
+
+% Hayama 18 data
+%FSFG6 (NMR)0.56 ± 0.01 (ITC)0.53 ± 0.07 mM
+%FSFG12 (NMR)0.37 ± 0.01 (ITC)0.42 ± 0.03 mM
+% combine as weighted averages?
+% cct1 KD
+line([560 560], [0 20]);
+% cct2 KD
+line([370 370], [0 20]);
+%% Plot of tethered diffusion prediction DB vs KD, separate subplots
+kd = logspace(1, 3); % in uM
+kon = 1e9*1e-6; %in uM^-1 s^-1;
+lp = 1e-3; % in um
+
+% plot cct1 values
+DF = 35; % in um^2/s
+Lc = 50e-3; % in um
+DB = DF./(1+3.*DF./(Lc.*lp.*kon.*kd));
+subplot(2,1,1)
+semilogx(kd, DB)
+% cct1 KD lower bound
+line([24 24], [0 20]);
+% cct1 KD upper bound
+line([300 300], [0 20]);
+% cct1 KD
+line([560 560], [0 20]);
+ylabel('D_B (\mum^2/s)');
+% cct1 DB
+line([10 1000], [5.62 5.62]);
+% cct1 DB upper error bar
+line([10 1000], [5.62+2.27 5.62+2.27]);
+% cct1 DB lower error bar
+line([10 1000], [5.62-2.27 5.62-2.27]);
+
+% plot cct2 values
+DF = 41; % in um^2/s
+Lc = 100e-3; % in um
+DB = DF./(1+3.*DF./(Lc.*lp.*kon.*kd));
+subplot(2,1,2)
+semilogx(kd, DB)
+% cct2 KD lower bound
+line([18 18], [0 20]);
+% cct2 KD upper bound
+line([180 180], [0 20]);
+% cct2 KD
+line([370 370], [0 20]);
+% cct2 DB
+line([10 1000], [5.22 5.22]);
+% cct2 DB upper error
+line([10 1000], [5.22+3.20 5.22+3.20]);
+% cct2 DB lower error
+line([10 1000], [5.22-3.20 5.22-3.20]);
+xlabel('Dissociation constant K_D (\muM)');
+ylabel('D_B (\mum^2/s)');
+
+
+% Hayama 18 data
+%FSFG6 (NMR)0.56 ± 0.01 (ITC)0.53 ± 0.07 mM
+%FSFG12 (NMR)0.37 ± 0.01 (ITC)0.42 ± 0.03 mM
+% combine as weighted averages?
+
---
+++ figs/ch05/Matlab scripts/FG124_fluorimetry_plots.m
@@ -0,0 +1,96 @@
+%% Script for producing plots from FG124 fluorimetry data
+
+%% PEG and PVP absorbance as a function of wavelength
+hold on
+plot(lambda,abs_PTB_PEG5percent,'o','MarkerFaceColor', [0, 0.4470, 0.7410]);
+plot(lambda,abs_PTB_PVP5percent,'o');
+hold off
+legend({'5% PEG in PTB', '5% PVP in PTB'});
+title('Crowder absorbance');
+xlabel('Wavelength (nm)');
+ylabel('Absorbance');
+str = 'Normalized by subtracting PTB absorbance.';
+annotation('textbox','String',str);
+
+%% Fresh and aggregated FG124 data in separate subplots
+subplot(1,3,1)
+hold on
+plot(lambda,FG124_PTB_fresh-blank_PTB,'--','Color',[0, 0.4470, 0.7410]);
+plot(lambda,FG124_PTB_agted-blank_PTB,'-','Color',[0, 0.4470, 0.7410]);
+axis([270 350 0 7.5e5])
+xlabel('Wavelength (nm)');
+ylabel('Intensity (counts)');
+legend({'340 $\mu$M fresh FG124','170 $\mu$M aggregated FG124'});
+title('PTB only')
+hold off
+subplot(1,3,2)
+hold on
+plot(lambda,FG124_PEG_fresh-blank_PEG,'--','Color',[0.8500, 0.3250, 0.0980]);
+plot(lambda,FG124_PEG_agted-blank_PEG,'-','Color',[0.8500, 0.3250, 0.0980]);
+axis([270 350 0 7.5e5])
+xlabel('Wavelength (nm)');
+legend({'340 $\mu$M fresh FG124','340 $\mu$M aggregated FG124'});
+title('5% PEG in PTB')
+hold off
+subplot(1,3,3)
+hold on
+plot(lambda,FG124_PVP_fresh-blank_PVP,'--','Color',[0.4940, 0.1840, 0.5560]);
+plot(lambda,FG124_PVP_agted-blank_PVP,'-','Color',[0.4940, 0.1840, 0.5560]);
+axis([270 350 0 7.5e5])
+xlabel('Wavelength (nm)');
+legend({'340 $\mu$M fresh FG124','340 $\mu$M aggregated FG124'});
+title('5% PVP in PTB')
+hold off
+
+%% Fresh and aggregated FG124 data normalized and offset
+hold on
+plot(lambda,(FG124_PTB_fresh-blank_PTB)/max(FG124_PTB_fresh-blank_PTB),...
+    '--','Color',[0, 0.4470, 0.7410]);
+plot(lambda,(FG124_PTB_agted-blank_PTB)/max(FG124_PTB_agted-blank_PTB),...
+    '-','Color',[0, 0.4470, 0.7410]);
+plot(lambda,(FG124_PEG_fresh-blank_PEG)/max(FG124_PEG_fresh-blank_PEG)+1,...
+    '--','Color',[0.8500, 0.3250, 0.0980]);
+plot(lambda,(FG124_PEG_agted-blank_PEG)/max(FG124_PEG_agted-blank_PEG)+1,...
+    '-','Color',[0.8500, 0.3250, 0.0980]);
+plot(lambda,(FG124_PVP_fresh-blank_PVP)/max(FG124_PVP_fresh-blank_PVP)+2,...
+    '--','Color',[0.4940, 0.1840, 0.5560]);
+plot(lambda,(FG124_PVP_agted-blank_PVP)/max(FG124_PVP_agted-blank_PVP)+2,...
+    '-','Color',[0.4940, 0.1840, 0.5560]);
+axis([270 350 0 3.05])
+xlabel('Wavelength (nm)');
+ylabel('Normalized ntensity (a.u.)');
+legend({'340 $\mu$M fresh FG124 in PTB','170 $\mu$M aggregated FG124 in PTB',...
+    '340 $\mu$M fresh FG124 in 5\% PEG','340 $\mu$M aggregated FG124 in 5\% PEG',...
+    '340 $\mu$M fresh FG124 in 5\% PVP','340 $\mu$M aggregated FG124 in 5\% PVP'},...
+    'Location','eastoutside');
+
+hold off
+
+%% Make plot comparing Phe peak (from reference) to FSFG and FG124
+hold on
+plot(Phe_lambda,Phe_notMine/max(Phe_notMine));
+plot(lambda,FSFG/max(FSFG));
+plot(lambda(1:81),FG124_fresh/max(FG124_fresh));
+plot(lambda(1:81),FG124_agted/max(FG124_agted));
+axis([270 350 0 1.05])
+xlabel('Wavelength (nm)');
+ylabel('Normalized ntensity (a.u.)');
+legend({'Phenylalanine (from ref.)','100 $\mu$M FSFG',...
+    '340 $\mu$M fresh FG124','170 $\mu$M aggregated FG124'},...
+    'Location','northeast');
+
+hold off
+
+%% Make plot of PEG and PVP only to show background
+
+hold on
+plot(lambda,blank_PEG);
+plot(lambda,blank_PVP);
+%plot(lambda(1:81),FG124_fresh/max(FG124_fresh));
+%plot(lambda(1:81),FG124_agted/max(FG124_agted));
+axis([270 350 0 2.5e5])
+xlabel('Wavelength (nm)');
+ylabel('Intensity (counts)');
+legend({'5% PEG in PTB','5% PVP in PTB'},'Location','northwest');
+
+hold off
